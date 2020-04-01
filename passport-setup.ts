e34e558bb7a528ded7139dd63bc52517280306ac
Rafat Sarosh <rsarosh@hotmail.com>
2020-04-01 07:07:01 -0700

    No admin access

--- passport-setup.ts
+++ passport-setup.ts
@@ -78,7 +78,10 @@ passport.use(
     {
       clientID: process.env.GITHUB_ClientID, //keys.github.clientID,
       clientSecret: process.env.GITHUB_ClientSecret,
-      scope: 'repo user admin:org read:org admin:org_hook admin:repo_hook read:repo_hook write:repo_hook',
+      //scope: 'repo user admin:org read:org admin:org_hook admin:repo_hook read:repo_hook write:repo_hook',
+      //https://developer.github.com/v3/orgs/hooks/
+      //no need to have a admin access
+      scope: 'repo user read:org read:repo_hook write:repo_hook',
     },
     (accessToken: any, refreshToken: any, profile: any, done: any) => {
       //Callback with the accessToken
